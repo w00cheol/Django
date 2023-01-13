9287f8ba3f19daa897e19d1d0e10c07badaf831a
w00cheol <kw8384@gmail.com>
2023-01-13 21:53:31 +0900

    UPDATE class-based view

--- tutorial-2/django-project/apps/voteapp/urls.py
+++ tutorial-2/django-project/apps/voteapp/urls.py
@@ -4,8 +4,17 @@ from . import views
 app_name = 'vote'
 
 urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # Class-based View
+
+    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'), # 매개변수 이름을 pk 로
+    re_path(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'), # 매개변수 이름을 pk 로
     re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    
+    # Function-based View
+
+    # re_path(r'^$', views.index, name='index'),
+    # re_path(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- tutorial-2/django-project/apps/voteapp/views.py
+++ tutorial-2/django-project/apps/voteapp/views.py
@@ -1,8 +1,31 @@
+from operator import ge
+from typing import overload
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
-from .models import Question, Choice
 
-# Create your views here.
+from .models import Question, Choice
+from django.views import generic
+
+# Class-based View
+class IndexView(generic.ListView):
+    template_name = 'voteapp/index.html'
+    context_object_name = 'latest_question_list' # 템플릿에 보낼 파라미터의 이름 지정
+
+    # 오버라이딩
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    # 매개변수 이름이 pk로 넘어오기 때문에 Question 모델 중에서 question 객체를 찾음
+    model = Question
+    template_name = 'voteapp/detail.html'
+
+class ResultsView(generic.DetailView):
+    # 매개변수 이름이 pk로 넘어오기 때문에 Question 모델 중에서 question 객체를 찾음
+    model = Question
+    template_name = 'voteapp/results.html'
+
+# Function-based View
 def index(req):
     # '-' => desc
     latest_question_list = Question.objects.order_by('-pub_date')[:3]
@@ -28,7 +51,7 @@ def vote(req, question_id):
             choice = q.choice_set.get(pk = req.POST['choice'])
             choice.votes += 1
             choice.save()
-            return redirect('vote:results', question_id = q.id)
+            return redirect('vote:results', pk = q.id)
 
         except Question.DoesNotExist:
             raise Http404(f"Question {question_id} does not exist")
